5cc0687472b419ff722713146646874e115e6e9b
Rick van Dam <rick-van-dam@outlook.com>
2025-07-29 21:02:12 +0200

    rename project and format code

--- Main.cs
+++ Main.cs
@@ -3,68 +3,67 @@ using System;
 
 public partial class Main : Node
 {
-		[Export]
-	public PackedScene MobScene { get; set; }
+    [Export] public PackedScene MobScene { get; set; }
 
-	private int _score;
-	
-	public override void _Ready()
-{
-	NewGame();
-}
-	
-	public void GameOver()
-{
-	GetNode<Timer>("MobTimer").Stop();
-	GetNode<Timer>("ScoreTimer").Stop();
-}
+    private int _score;
 
-public void NewGame()
-{
-	_score = 0;
+    public override void _Ready()
+    {
+        NewGame();
+    }
 
-	var player = GetNode<Player>("Player");
-	var startPosition = GetNode<Marker2D>("StartPosition");
-	player.Start(startPosition.Position);
+    public void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
 
-	GetNode<Timer>("StartTimer").Start();
-}
+    public void NewGame()
+    {
+        _score = 0;
 
-private void OnScoreTimerTimeout()
-{
-	_score++;
-}
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Marker2D>("StartPosition");
+        player.Start(startPosition.Position);
 
-private void OnStartTimerTimeout()
-{
-	GetNode<Timer>("MobTimer").Start();
-	GetNode<Timer>("ScoreTimer").Start();
-}
+        GetNode<Timer>("StartTimer").Start();
+    }
 
-private void OnMobTimerTimeout()
-{
-	// Create a new instance of the Mob scene.
-	Mob mob = MobScene.Instantiate<Mob>();
+    private void OnScoreTimerTimeout()
+    {
+        _score++;
+    }
 
-	// Choose a random location on Path2D.
-	var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
-	mobSpawnLocation.ProgressRatio = GD.Randf();
+    private void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
 
-	// Set the mob's direction perpendicular to the path direction.
-	float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+    private void OnMobTimerTimeout()
+    {
+        // Create a new instance of the Mob scene.
+        Mob mob = MobScene.Instantiate<Mob>();
 
-	// Set the mob's position to a random location.
-	mob.Position = mobSpawnLocation.Position;
+        // Choose a random location on Path2D.
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.ProgressRatio = GD.Randf();
 
-	// Add some randomness to the direction.
-	direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
-	mob.Rotation = direction;
+        // Set the mob's direction perpendicular to the path direction.
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
 
-	// Choose the velocity.
-	var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
-	mob.LinearVelocity = velocity.Rotated(direction);
+        // Set the mob's position to a random location.
+        mob.Position = mobSpawnLocation.Position;
 
-	// Spawn the mob by adding it to the Main scene.
-	AddChild(mob);
-}
+        // Add some randomness to the direction.
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        // Choose the velocity.
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
 }
--- Mob.cs
+++ Mob.cs
@@ -3,15 +3,15 @@ using System;
 
 public partial class Mob : RigidBody2D
 {
-	public override void _Ready()
-{
-	var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-	string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
-	animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
-}
+    public override void _Ready()
+    {
+        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        string[] mobTypes = animatedSprite2D.SpriteFrames.GetAnimationNames();
+        animatedSprite2D.Play(mobTypes[GD.Randi() % mobTypes.Length]);
+    }
 
-private void OnVisibleOnScreenNotifier2DScreenExited()
-{
-	QueueFree();
-}
+    private void OnVisibleOnScreenNotifier2DScreenExited()
+    {
+        QueueFree();
+    }
 }
